9eb0f4e67670c691b851ed3f5493f966e7d97025
logan mohseni <mohsenil85@gmail.com>
2012-12-14 14:55:24 -0700

    working with args

--- main.hs
+++ main.hs
@@ -2,7 +2,11 @@ module Main where
 
 import System.Environment
 
+bob x = x * 2
+
 main :: IO ()
 main = do
     args <- getArgs
     putStrLn ("i'm sorry, " ++ args !! 0 ++ ". i can't do that.")
+    bob  (args !! 1)
+    putStrLn ("fuck " ++ args !! 1 ++ "fuckity")
